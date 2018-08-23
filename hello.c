13a29dc417b5a93b02f9d701ddb85c0e8e6ecfff
AsagamiShao <42654718+AsagamiShao@users.noreply.github.com>
2018-08-24 03:53:40 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zhenghao Shao
+ * Date: 2018/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Zhenghao Shao!\n");
+
+  return 0;
+}
